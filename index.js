d8ce43290d8a8fccd5049e8c8fee613ecc64d385
Dustin McBride <dustinbm@gmail.com>
2016-05-16 11:22:54 -0700

    fixed lint erros added gulp

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-const bmp = require('./lib/bmpRGBtransformer.js')
+var bmp = require('./lib/bmpRGBtransformer.js');
 
-  filename = __dirname + "/" + process.argv[2];
-  if (process.argv[6]) {
-    output = __dirname + "/" + process.argv[6]
-  } else {
-    output = __dirname + "/bmp-trasformed.bmp"
-  }
-  rgb = [process.argv[3] || 1, process.argv[4] || 1, process.argv[5] || 1]
+filename = __dirname + '/' + process.argv[2];
+if (process.argv[6]) {
+  output = __dirname + '/' + process.argv[6];
+} else {
+  output = __dirname + '/bmp-trasformed.bmp';
+};
+rgb = [process.argv[3] || 1, process.argv[4] || 1, process.argv[5] || 1];
 
-bmp.bmpRGBtransformer(filename, output, rgb)
+bmp.bmpRGBtransformer(filename, output, rgb);
--- test/bitmapRGBtransformer_test.js
+++ test/bitmapRGBtransformer_test.js
@@ -1,65 +1,65 @@
-const chai = require('chai');
-const expect = chai.expect;
-const bmp = require('../lib/bmpRGBtransformer.js')
-const fs = require('fs');
+var chai = require('chai');
+var expect = chai.expect;
+var bmp = require('../lib/bmpRGBtransformer.js');
+var fs = require('fs');
 var fileExists = require('file-exists');
 
-describe('Testing 8 bit transform with out arguments', () => {
-  var testbmp = __dirname + '/testimages/dude-8bit.bmp'
-  var bmpoutput = __dirname + '/testimages/test-dude-8bit.bmp'
-  var rgb = [1, 1, 1]
+describe('Testing 8 bit transform with out arguments', function () {
+  var testbmp = __dirname + '/testimages/dude-8bit.bmp';
+  var bmpoutput = __dirname + '/testimages/test-dude-8bit.bmp';
+  var rgb = [1, 1, 1];
 
-  before((done) => {
-    bmp.bmpRGBtransformer(testbmp, bmpoutput, rgb, done)
+  before ( function (done) {
+    bmp.bmpRGBtransformer(testbmp, bmpoutput, rgb, done);
   });
 
   it('should return a new file', function () {
-    expect(fileExists(bmpoutput)).to.eql(true)
+    expect(fileExists(bmpoutput)).to.eql(true);
   });
 
   it('should be an exact copy of orignal file', function () {
-    a = fs.readFileSync(testbmp)
-    b = fs.readFileSync(bmpoutput)
-    expect(a).to.eql(a)
+    a = fs.readFileSync(testbmp);
+    b = fs.readFileSync(bmpoutput);
+    expect(a).to.eql(a);
   });
 });
 
-describe('Testing 16 bit transform with out arguments', () => {
-  var testbmp = __dirname + '/testimages/dude-16bit.bmp'
-  var bmpoutput = __dirname + '/testimages/test-dude-16bit.bmp'
-  var rgb = [1, 1, 1]
+describe('Testing 16 bit transform with out arguments', function () {
+  var testbmp = __dirname + '/testimages/dude-16bit.bmp';
+  var bmpoutput = __dirname + '/testimages/test-dude-16bit.bmp';
+  var rgb = [1, 1, 1];
 
-  before((done) => {
-    bmp.bmpRGBtransformer(testbmp, bmpoutput, rgb, done)
+  before( function (done) {
+    bmp.bmpRGBtransformer(testbmp, bmpoutput, rgb, done);
   });
 
   it('should return a new file', function () {
-    expect(fileExists(bmpoutput)).to.eql(true)
+    expect(fileExists(bmpoutput)).to.eql(true);
   });
 
   it('should be an exact copy of orignal file', function () {
-    a = fs.readFileSync(testbmp)
-    b = fs.readFileSync(bmpoutput)
-    expect(a).to.eql(a)
+    a = fs.readFileSync(testbmp);
+    b = fs.readFileSync(bmpoutput);
+    expect(a).to.eql(a);
   });
 });
 
-describe('Testing 24 bit transform with out arguments', () => {
-  var testbmp = __dirname + '/testimages/dude-24bit.bmp'
-  var bmpoutput = __dirname + '/testimages/test-dude-24bit.bmp'
-  var rgb = [1, 1, 1]
+describe('Testing 24 bit transform with out arguments', function () {
+  var testbmp = __dirname + '/testimages/dude-24bit.bmp';
+  var bmpoutput = __dirname + '/testimages/test-dude-24bit.bmp';
+  var rgb = [1, 1, 1];
 
-  before((done) => {
-    bmp.bmpRGBtransformer(testbmp, bmpoutput, rgb, done)
+  before( function (done) {
+    bmp.bmpRGBtransformer(testbmp, bmpoutput, rgb, done);
   });
 
   it('should return a new file', function () {
-    expect(fileExists(bmpoutput)).to.eql(true)
+    expect(fileExists(bmpoutput)).to.eql(true);
   });
 
   it('should be an exact copy of orignal file', function () {
-    a = fs.readFileSync(testbmp)
-    b = fs.readFileSync(bmpoutput)
-    expect(a).to.eql(a)
+    a = fs.readFileSync(testbmp);
+    b = fs.readFileSync(bmpoutput);
+    expect(a).to.eql(a);
   });
 });
